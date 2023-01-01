a7ce5af69985addd5aa01fc7389341bb230ed7dd
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-01-01 22:24:36 +0300

    Project init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { LoginComponent } from './feature/login/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AseLayout from './general/AseLayout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AseLayout>
+        <Routes>
+          <Route exact path="/" element={<LoginComponent />} />
+          <Route exact path="/home" element={<h1>Deneme</h1>} />
+        </Routes>
+      </AseLayout>
+    </BrowserRouter>
   );
 }
 
